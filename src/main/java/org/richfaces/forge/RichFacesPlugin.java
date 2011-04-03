41c1fb3c3e7dc563bcff36a6b4dceed9fe68badd
Brian Leathem <bleathem@gmail.com>
2011-04-03 14:23:54 -0700

    Defined a managed bean to back the Richfaces enabled example facelet

--- src/main/java/org/richfaces/forge/RichFacesPlugin.java
+++ src/main/java/org/richfaces/forge/RichFacesPlugin.java
@@ -1,14 +1,6 @@
 package org.richfaces.forge;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.faces.webapp.FacesServlet;
 import javax.inject.Inject;
 import org.apache.maven.model.DependencyManagement;
@@ -22,6 +14,7 @@ import org.jboss.seam.forge.project.facets.DependencyFacet;
 import org.jboss.seam.forge.project.facets.MavenCoreFacet;
 import org.jboss.seam.forge.project.facets.WebResourceFacet;
 import org.jboss.seam.forge.project.facets.builtin.MavenDependencyFacet;
+import org.jboss.seam.forge.project.facets.builtin.MavenJavaSourceFacet;
 import org.jboss.seam.forge.resources.DirectoryResource;
 import org.jboss.seam.forge.resources.FileResource;
 import org.jboss.seam.forge.shell.ShellColor;
@@ -42,18 +35,46 @@ public class RichFacesPlugin implements Plugin {
     private static final String RICHFACES_VERSION = "4.0.0.Final";
     private static final String SUCCESS_MSG_FMT = "***SUCCESS*** %s %s has been installed.";
     private static final String ALREADY_INSTALLED_MSG_FMT = "***INFO*** %s %s is already present.";
-    
+
     @Inject
     @ProjectScoped Project project;
-    
+
     @DefaultCommand
     public void exampleDefaultCommand(@Option String opt, PipeOut pipeOut) {
-        pipeOut.println(ShellColor.BLUE, "Use the install command to add a Seam Faces dependency.");
+        pipeOut.println(ShellColor.BLUE, "Use the install commands to install:");
+        pipeOut.println(ShellColor.BLUE, "  install-base: maven dependencies, and web.xml configuration");
+        pipeOut.println(ShellColor.BLUE, "  install-example-facelet: base, as well asa a sample RichFaces enabled facelet file");
+        pipeOut.println(ShellColor.BLUE, "  install: same as install-example-facelet");
     }
-    
+
+
     @Command("install")
     public void installCommand(PipeOut pipeOut) {
         installDependencies(pipeOut);
+        installDescriptor(pipeOut);
+        createFaceletFiles(pipeOut);
+        createRichBean(pipeOut);
+    }
+
+    @Command("install-base")
+    public void installBase(PipeOut pipeOut) {
+        installDependencies(pipeOut);
+        installDescriptor(pipeOut);
+    }
+
+    @Command("install-example-facelet")
+    public void installExampleFacelets(PipeOut pipeOut) {
+        installDependencies(pipeOut);  // make sure the
+        installDescriptor(pipeOut);
+        createFaceletFiles(pipeOut);
+        createRichBean(pipeOut);
+    }
+    /**
+     * Set the context-params and Servlet definition if they are not yet set.
+     *
+     * @param pipeOut
+     */
+    private void installDescriptor(PipeOut pipeOut) {
         ServletFacet servlet = project.getFacet(ServletFacet.class);
         WebAppDescriptor descriptor = servlet.getConfig();
         if (descriptor.getContextParam("javax.faces.PROJECT_STAGE") == null) {
@@ -68,38 +89,31 @@ public class RichFacesPlugin implements Plugin {
         } else {
             pipeOut.println(ShellColor.YELLOW, String.format(ALREADY_INSTALLED_MSG_FMT, "javax.faces.SKIP_COMMENTS", "context-param"));
         }
-        installFacesServlet(descriptor, pipeOut);
+
+        if (isFacesServletDefined(descriptor)) {
+            pipeOut.println(ShellColor.YELLOW, String.format(ALREADY_INSTALLED_MSG_FMT, "Faces Servlet", "mapping"));
+        } else {
+            descriptor.servlet("Faces Servlet", FacesServlet.class, new String[] {"*.jsf", "/faces/*"});
+            pipeOut.println(ShellColor.GREEN, String.format(SUCCESS_MSG_FMT, "Faces Servlet", "mapping"));
+        }
+
         descriptor.sessionTimeout(30);
-//        TODO: 
+//        TODO:
 //        <mime-mapping>
 //            <extension>ecss</extension>
 //            <mime-type>text/css</mime-type>
 //        </mime-mapping>
         descriptor.welcomeFile("faces/index.xhtml");
         servlet.saveConfig(descriptor);
-        createFaceletFiles(pipeOut);
     }
-    
-    private void createFaceletFiles(PipeOut pipeOut) {
-        DirectoryResource webRoot = project.getFacet(WebResourceFacet.class).getWebRootDirectory();
-        DirectoryResource templateDirectory = webRoot.getOrCreateChildDirectory("templates");
-        FileResource<?> templatePage = (FileResource<?>) templateDirectory.getChild("template.xhtml");
-        InputStream stream = RichFacesPlugin.class.getResourceAsStream("/org/richfaces/forge/template.xhtml");
-        templatePage.setContents(stream);
-        pipeOut.println(ShellColor.YELLOW, String.format(SUCCESS_MSG_FMT, "template.xhtml", "file"));
-        
-        FileResource<?> indexPage = (FileResource<?>) webRoot.getChild("index.xhtml");
-        stream = RichFacesPlugin.class.getResourceAsStream("/org/richfaces/forge/index.xhtml");
-        indexPage.setContents(stream);
-        pipeOut.println(ShellColor.YELLOW, String.format(SUCCESS_MSG_FMT, "index.xhtml", "file"));
-        
-        FileResource<?> forgeIndexPage = (FileResource<?>) webRoot.getChild("index.html");
-        String contents;
-//        TODO: if (contents.contains("Welcome to Seam Forge")) {
-        forgeIndexPage.delete();
-    }
-    
-    private void installFacesServlet(WebAppDescriptor descriptor, PipeOut pipeOut) {
+
+    /**
+     * A helper method to determine if the Faces Servlet is defined in the web.xml
+     *
+     * @param descriptor
+     * @return true if the Faces Servlet is defined, false otherwise
+     */
+    private boolean isFacesServletDefined(WebAppDescriptor descriptor) {
 //        TODO: When WebAppDescriptor.getServlets is implemented:
 //        List<ServletDef> servlets = descriptor.getServlets();
 //        if (servlets != null && ! servlets.isEmpty()) {
@@ -113,51 +127,57 @@ public class RichFacesPlugin implements Plugin {
 //        } else {
 //            pipeOut.println("servlets list is empty");
 //        }
-        if (descriptor.exportAsString().contains(FacesServlet.class.getName())) {
-            pipeOut.println(ShellColor.YELLOW, String.format(ALREADY_INSTALLED_MSG_FMT, "Faces Servlet", "mapping"));
-            return;
-        }
-        descriptor.servlet("Faces Servlet", FacesServlet.class, new String[] {"*.jsf", "/faces/*"});
-        pipeOut.println(ShellColor.GREEN, String.format(SUCCESS_MSG_FMT, "Faces Servlet", "mapping"));
+        return descriptor.exportAsString().contains(FacesServlet.class.getName());
     }
-    
+
+    /**
+     * Install the maven dependencies required for RichFaces
+     *
+     * @param pipeOut
+     */
     private void installDependencies(PipeOut pipeOut) {
         installDependencyManagement(project, pipeOut);
-        
+
         DependencyFacet deps = project.getFacet(DependencyFacet.class);
         DependencyBuilder dependency;
-        
+
         dependency = DependencyBuilder.create();
         dependency.setArtifactId("richfaces-components-ui").setGroupId("org.richfaces.ui").setVersion(RICHFACES_VERSION);
         installDependency(deps, dependency, pipeOut);
-        
+
         dependency = DependencyBuilder.create();
         dependency.setArtifactId("richfaces-core-impl").setGroupId("org.richfaces.core").setVersion(RICHFACES_VERSION);
         installDependency(deps, dependency, pipeOut);
-        
+
         dependency = DependencyBuilder.create();
         dependency.setArtifactId("servlet-api").setGroupId("javax.servlet").setScopeType(ScopeType.PROVIDED);
         installDependency(deps, dependency, pipeOut);
-        
+
         dependency = DependencyBuilder.create();
         dependency.setArtifactId("jsp-api").setGroupId("javax.servlet.jsp").setScopeType(ScopeType.PROVIDED);
         installDependency(deps, dependency, pipeOut);
-        
+
         dependency = DependencyBuilder.create();
         dependency.setArtifactId("jstl").setGroupId("javax.servlet").setScopeType(ScopeType.PROVIDED);
         installDependency(deps, dependency, pipeOut);
-        
+
         dependency = DependencyBuilder.create();
         dependency.setArtifactId("ehcache").setGroupId("net.sf.ehcache");
         installDependency(deps, dependency, pipeOut);
-        
+
 //        TODO: When forge has classifier support (<classifier>jdk15</classifier>)
 //        dependency = DependencyBuilder.create();
 //        dependency.setArtifactId("testng").setGroupId("org.testng").setVersion("5.1.0").setScopeType(ScopeType.TEST);
 //        installDependency(deps, dependency, pipeOut);
-        
+
     }
-    
+
+    /**
+     * Install the richfaces-bom in the pom's dependency management
+     *
+     * @param project
+     * @param pipeOut
+     */
     private void installDependencyManagement(Project project, PipeOut pipeOut) {
         MavenCoreFacet maven = project.getFacet(MavenCoreFacet.class);
         Model pom = maven.getPOM();
@@ -178,10 +198,17 @@ public class RichFacesPlugin implements Plugin {
         dependencyManagement.addDependency(new MavenDependencyAdapter(bomDependency));
         pom.setDependencyManagement(dependencyManagement);
         maven.setPOM(pom);
-        
+
         pipeOut.println(ShellColor.GREEN, String.format(SUCCESS_MSG_FMT, "RichFaces BOM", "dependency"));
     }
-    
+
+    /**
+     * A helper method to install a dependency, and log the result
+     *
+     * @param deps
+     * @param dependency
+     * @param pipeOut
+     */
     private void installDependency(DependencyFacet deps, Dependency dependency, PipeOut pipeOut) {
         String name = dependency.toCoordinates();
         if (deps.hasDependency(dependency)) {
@@ -191,10 +218,43 @@ public class RichFacesPlugin implements Plugin {
             pipeOut.println(ShellColor.GREEN, String.format(SUCCESS_MSG_FMT, name, "dependency"));
         }
     }
-    
-    private void installrichBean() {
-    
-    }
-    
-}
 
+    /**
+     * Create a simple template file, and a RichFaces enabled index file that uses the template
+     *
+     * @param pipeOut
+     */
+    private void createFaceletFiles(PipeOut pipeOut) {
+        DirectoryResource webRoot = project.getFacet(WebResourceFacet.class).getWebRootDirectory();
+        DirectoryResource templateDirectory = webRoot.getOrCreateChildDirectory("templates");
+        FileResource<?> templatePage = (FileResource<?>) templateDirectory.getChild("template.xhtml");
+        InputStream stream = RichFacesPlugin.class.getResourceAsStream("/org/richfaces/forge/template.xhtml");
+        templatePage.setContents(stream);
+        pipeOut.println(ShellColor.YELLOW, String.format(SUCCESS_MSG_FMT, "template.xhtml", "file"));
+
+        FileResource<?> indexPage = (FileResource<?>) webRoot.getChild("index.xhtml");
+        stream = RichFacesPlugin.class.getResourceAsStream("/org/richfaces/forge/index.xhtml");
+        indexPage.setContents(stream);
+        pipeOut.println(ShellColor.YELLOW, String.format(SUCCESS_MSG_FMT, "index.xhtml", "file"));
+
+        FileResource<?> forgeIndexPage = (FileResource<?>) webRoot.getChild("index.html");
+        String contents;
+//        TODO: if (contents.contains("Welcome to Seam Forge")) {
+        forgeIndexPage.delete();
+    }
+
+    /**
+     * Create a simple JSF managed bean to back the RichFaces input in the example facelet file
+     *
+     * @param pipeOut
+     */
+    private void createRichBean(PipeOut pipeOut) {
+        MavenJavaSourceFacet source = project.getFacet(MavenJavaSourceFacet.class);
+        DirectoryResource sourceRoot = source.getBasePackageResource();
+        FileResource<?> indexPage = (FileResource<?>) sourceRoot.getChild("RichBean.java");
+        InputStream stream = RichFacesPlugin.class.getResourceAsStream("/org/richfaces/forge/RichBean.java.txt");
+        indexPage.setContents(stream);
+        pipeOut.println(ShellColor.YELLOW, String.format(SUCCESS_MSG_FMT, "RichBean", "class"));
+    }
+
+}
---
+++ src/main/resources/org/richfaces/forge/RichBean.java.txt
@@ -0,0 +1,26 @@
+package ca.bleathem.richfaces;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ViewScoped
+@ManagedBean(name="richBean")
+public class RichBean implements Serializable {
+
+    private static final long serialVersionUID = -2403138958014741653L;
+    private String name;
+
+    public RichBean() {
+        System.out.println("post construct: initialize");
+        name = "John";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
