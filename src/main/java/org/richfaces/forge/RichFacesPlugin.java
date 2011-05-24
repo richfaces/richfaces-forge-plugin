8b85e6e419dbdb761a106c86bee71d3ee2bdeaf1
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2011-05-24 19:49:37 -0400

    Updated for new Forge API

--- src/main/java/org/richfaces/forge/RichFacesPlugin.java
+++ src/main/java/org/richfaces/forge/RichFacesPlugin.java
@@ -8,11 +8,11 @@ import javax.inject.Inject;
 import org.jboss.forge.project.Project;
 import org.jboss.forge.project.facets.JavaSourceFacet;
 import org.jboss.forge.project.facets.WebResourceFacet;
+import org.jboss.forge.project.facets.events.InstallFacets;
 import org.jboss.forge.resources.DirectoryResource;
 import org.jboss.forge.resources.FileResource;
 import org.jboss.forge.shell.ShellColor;
 import org.jboss.forge.shell.ShellMessages;
-import org.jboss.forge.shell.events.InstallFacets;
 import org.jboss.forge.shell.plugins.Alias;
 import org.jboss.forge.shell.plugins.Command;
 import org.jboss.forge.shell.plugins.DefaultCommand;
@@ -30,14 +30,14 @@ public class RichFacesPlugin implements Plugin
    private final Event<InstallFacets> installFacets;
 
    @Inject
-   public RichFacesPlugin(Project project, Event<InstallFacets> event)
+   public RichFacesPlugin(final Project project, final Event<InstallFacets> event)
    {
       this.project = project;
       this.installFacets = event;
    }
 
    @DefaultCommand
-   public void status(PipeOut out)
+   public void status(final PipeOut out)
    {
       if (project.hasFacet(RichFacesFacet.class))
       {
@@ -51,7 +51,7 @@ public class RichFacesPlugin implements Plugin
 
    // confirmed working
    @Command("setup")
-   public void setup(PipeOut out)
+   public void setup(final PipeOut out)
    {
       if (!project.hasFacet(RichFacesFacet.class))
       {
@@ -72,14 +72,14 @@ public class RichFacesPlugin implements Plugin
    }
 
    @Command("help")
-   public void exampleDefaultCommand(@Option String opt, PipeOut pipeOut)
+   public void exampleDefaultCommand(@Option final String opt, final PipeOut pipeOut)
    {
       pipeOut.println(ShellColor.BLUE, "Use the install commands to install:");
       pipeOut.println(ShellColor.BLUE, "  install-example-facelet: a sample RichFaces enabled facelet file");
    }
 
    @Command("install-example-facelet")
-   public void installExampleFacelets(PipeOut pipeOut)
+   public void installExampleFacelets(final PipeOut pipeOut)
    {
       assertInstalled();
       createFaceletFiles(pipeOut);
@@ -91,7 +91,7 @@ public class RichFacesPlugin implements Plugin
     * 
     * @param pipeOut
     */
-   private void createFaceletFiles(PipeOut pipeOut)
+   private void createFaceletFiles(final PipeOut pipeOut)
    {
       DirectoryResource webRoot = project.getFacet(WebResourceFacet.class).getWebRootDirectory();
       DirectoryResource templateDirectory = webRoot.getOrCreateChildDirectory("templates");
@@ -116,7 +116,7 @@ public class RichFacesPlugin implements Plugin
     * 
     * @param pipeOut
     */
-   private void createRichBean(PipeOut pipeOut)
+   private void createRichBean(final PipeOut pipeOut)
    {
       JavaSourceFacet source = project.getFacet(JavaSourceFacet.class);
       DirectoryResource sourceRoot = source.getBasePackageResource();
